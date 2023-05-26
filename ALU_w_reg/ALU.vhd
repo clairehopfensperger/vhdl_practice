65d4e1a970b03f64d7d51f30bbdcb0b35dfa8e30
Claire Hopfensperger <99447241+clairehopfensperger@users.noreply.github.com>
2023-05-26 13:52:06 -0400

    clocked ALU to fix program
    had to clock ALU, i don't understand why, but this change fixed the entire program

--- ALU_w_reg/ALU.vhd
+++ ALU_w_reg/ALU.vhd
@@ -32,68 +32,67 @@ use IEEE.NUMERIC_STD.ALL;
 --use UNISIM.VComponents.all;
 
 entity ALU is
-    generic (
-        add     : std_logic_vector(2 downto 0) := "000";
-        sub     : std_logic_vector(2 downto 0) := "001";
-        gt      : std_logic_vector(2 downto 0) := "010";
-        eq      : std_logic_vector(2 downto 0) := "011";
-        b_xor   : std_logic_vector(2 downto 0) := "100";
-        b_and   : std_logic_vector(2 downto 0) := "101";
-        b_or    : std_logic_vector(2 downto 0) := "110";
-        neg     : std_logic_vector(2 downto 0) := "111"
-    );
-        
     port (
-        num_a   : in std_logic_vector(3 downto 0);
-        num_b   : in std_logic_vector(3 downto 0);
+        clk     : in std_logic;
+        num_a   : in std_logic_vector(5 downto 0);
+        num_b   : in std_logic_vector(5 downto 0);
         control : in std_logic_vector(2 downto 0);
-        result  : out std_logic_vector(3 downto 0)
+        result  : out std_logic_vector(5 downto 0)
     );
 end ALU;
 
 architecture arch_ALU of ALU is
-
+    constant add     : std_logic_vector(2 downto 0) := "000";
+    constant sub     : std_logic_vector(2 downto 0) := "001";
+    constant gt      : std_logic_vector(2 downto 0) := "010";
+    constant eq      : std_logic_vector(2 downto 0) := "011";
+    constant b_xor   : std_logic_vector(2 downto 0) := "100";
+    constant b_and   : std_logic_vector(2 downto 0) := "101";
+    constant b_or    : std_logic_vector(2 downto 0) := "110";
+    constant neg     : std_logic_vector(2 downto 0) := "111";
 begin
     
     -- case statement 
-    process (control) is
+    process (clk, control) is
     begin 
-        case control is
-            when add =>
-                result <= std_logic_vector(unsigned(num_a) + unsigned(num_b));
-            when sub =>
-                result <= std_logic_vector(unsigned(num_a) - unsigned(num_b));
-            when gt =>
-                if num_a(3) = '1' and num_b(3) = '0' then
-                    result <= "0000";
-                elsif num_a(3) = '0' and num_b(3) = '1' then
-                    result <= "0001";
-                else 
-                    --result <= std_logic_vector(unsigned(num_a) > unsigned(num_b));  -- error
-                    if num_a > num_b then 
-                        result <= "0001";
-                    else
-                        result <= "0000";
+        if (rising_edge(clk)) then
+            case control is
+                when add =>
+                    result <= std_logic_vector(unsigned(num_a) + unsigned(num_b));
+                when sub =>
+                    result <= std_logic_vector(unsigned(num_a) - unsigned(num_b));
+                when gt =>
+                    if num_a(3) = '1' and num_b(3) = '0' then
+                        result <= "000000";
+                    elsif num_a(3) = '0' and num_b(3) = '1' then
+                        result <= "000001";
+                    else 
+                        --result <= std_logic_vector(unsigned(num_a) > unsigned(num_b));  -- error
+                        if num_a > num_b then 
+                            result <= "000001";
+                        else
+                            result <= "000000";
+                        end if;
                     end if;
-                end if;
-            when eq =>
-                --result <= std_logic_vector(unsigned(num_a) = unsigned(num_b));  -- error
-                if num_a = num_b then 
-                    result <= "0001";
-                else
-                    result <= "0000";
-                end if;
-            when b_xor =>
-                result <= num_a xor num_b;
-            when b_and =>
-                result <= num_a and num_b;
-            when b_or =>
-                result <= num_a or num_b;
-            when neg =>    
-                result <= not num_a;
-            when others => 
-                result <= "0000";
-        end case;
+                when eq =>
+                    --result <= std_logic_vector(unsigned(num_a) = unsigned(num_b));  -- error
+                    if num_a = num_b then 
+                        result <= "000001";
+                    else
+                        result <= "000000";
+                    end if;
+                when b_xor =>
+                    result <= num_a xor num_b;
+                when b_and =>
+                    result <= num_a and num_b;
+                when b_or =>
+                    result <= num_a or num_b;
+                when neg =>    
+                    result <= not num_a;
+                when others => 
+                    result <= "000000";
+            end case;
+        end if;
     end process;    
          
 end arch_ALU;
